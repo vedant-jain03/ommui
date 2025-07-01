02a0c443fdf825145772ad84461f3f0322277e93
vedant-jain03 <vedantjainben10@gmail.com>
2025-07-01 23:33:33 +0530

    add analytics

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
+import { inject } from "@vercel/analytics";
 
 const pinia = createPinia();
 const app = createApp(App);
+
+inject();
 
 app.use(pinia);
 app.mount("#app");
